4b010c0e8b4691c22e42d9d316e2c860916e51c8
Aleksandar Ignatov <aleksandar@mentormate.com>
2019-06-05 12:36:05 +0300

    Initial Commit

---
+++ Animated SetTests/Animated_SetTests.swift
@@ -0,0 +1,34 @@
+//
+//  Animated_SetTests.swift
+//  Animated SetTests
+//
+//  Created by Aleksandar Ignatov on 5.06.19.
+//  Copyright © 2019 MentorMate. All rights reserved.
+//
+
+import XCTest
+@testable import Animated_Set
+
+class Animated_SetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
